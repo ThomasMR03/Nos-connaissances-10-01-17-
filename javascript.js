bfad9534bdc66602f361414d3ecdc67ac06aa2a9
Thomas <Thomas.Face03@gmail.com>
2017-01-10 15:45:26 +0100

    Ajout de l'interaction avec l'user

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
-const SEUIL=200; //Déclare la constante
+var SEUIL=prompt ("Combien de case voulez-vous afficher ?");
 // var i = numCase
-
+	if (SEUIL<1000) { //Début de condition (Si le seuil dépasse 1000)
 for (var i = 1; i <= SEUIL; i++) {
 	document.getElementById('liste').innerHTML += '<li>'+i+'</li>'; //On change le HTML
+}}	else { //Fin de condition (Alors alert message user)
+	alert('Nombre trop élevé, veuillez réactuliser la page et donner un nombre entre 1 et 1000');
 }
