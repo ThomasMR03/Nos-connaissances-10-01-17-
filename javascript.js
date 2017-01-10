b5e7be927a494c115c0bf06ca48f110872d1105f
Thomas <Thomas.Face03@gmail.com>
2017-01-10 15:20:27 +0100

    Modification HTML CSS et ajout du JS

---
+++ javascript.js
@@ -0,0 +1,6 @@
+const SEUIL=200; //Déclare la constante
+// var i = numCase
+
+for (var i = 1; i <= SEUIL; i++) {
+	document.getElementById('liste').innerHTML += '<li>'+i+'</li>'; //On change le HTML
+}
